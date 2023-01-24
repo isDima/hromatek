7883557f3a2b65c3824cc7187f8d8879fe8f86a4
Дмитрий <96423128+isDima@users.noreply.github.com>
2023-01-24 18:26:00 +0300

    Update Combinations.cs

--- Combinations.cs
+++ Combinations.cs
@@ -1,24 +1,29 @@
 using System;
-namespace Combinations
+
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please, enter your string:");
-            char[] inputString = Console.ReadLine().ToCharArray();
-            int len = inputString.Length;
+            Console.WriteLine("Введите произвольную строку:");
+            string str = Console.ReadLine();
 
-            for (int i = 0; i < len; i++)
+            int n = str.Length;
+            int count = 0;
+            for (int i = 0; i < n-1; i++)
             {
-                for (int j = 0; j < len; j++)
+                for (int j = i + 1; j < n; j++)
                 {
-                    if (i <= j)
-                        Console.Write($"{inputString[i]}{inputString[j]} ");
+                    if (str[i] != str[j])
+                    {
+                        Console.WriteLine(str[i] + "" + str[j]);
+                        count += 1;
+                    }
                 }
             }
-            Console.WriteLine($"\nNumber of combinations: {(len * len + len) / 2}");
-
+            Console.WriteLine();
+            Console.WriteLine("Количество найденных комбинаций:" + count);
             Console.ReadKey();
         }
     }
